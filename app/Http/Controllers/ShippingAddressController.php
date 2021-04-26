4ca408be7b1cc5a07b3791ac5669c0324d6153d6
kisgabor1990 <kisgabor90@gmail.com>
2021-04-26 18:39:03 +0200

    Felhasználók adatbázis átalakítás
    Külön táblában kezelni a felhasználókat, és a hozzájuk tartozó információkat.

---
+++ app/Http/Controllers/ShippingAddressController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Shipping_address;
+use Illuminate\Http\Request;
+
+class ShippingAddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Shipping_address  $shipping_address
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Shipping_address $shipping_address)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Shipping_address  $shipping_address
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Shipping_address $shipping_address)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Shipping_address  $shipping_address
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Shipping_address $shipping_address)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Shipping_address  $shipping_address
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Shipping_address $shipping_address)
+    {
+        //
+    }
+}
