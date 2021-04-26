de9e2677e3c33f9b5ee2ec764f586e9dbf911354
kisgabor1990 <kisgabor90@gmail.com>
2021-04-26 18:39:46 +0200

    Csoportosítás
    Csoportokra bontás middleware szerint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OpinionController;
 use App\Http\Controllers\PagesController;
@@ -21,61 +22,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PagesController::class, 'index'] );
+Route::get('/', [PagesController::class, 'index']);
 
 Route::redirect('/home', '/');
 
-Route::get('/termekek', [ProductsController::class, 'index'] );
-Route::get('/termekek/{id}', [ProductsController::class, 'show'] )->whereNumber('id');
-Route::get('/termekek/{category}', [ProductsController::class, 'list'] )->where('category', '[a-z-]+');
-
-Route::post('/termekek/{id}/velemeny', [OpinionController::class, 'store'] );
-Route::get('/termekek/{id}/velemeny/szerkesztes', [OpinionController::class, 'edit'] )
-    ->middleware(['auth']);
-Route::post('/termekek/{id}/velemeny/modositas', [OpinionController::class, 'update'] );
-Route::get('/termekek/{id}/velemeny/torles', [OpinionController::class, 'destroy'] )
-->middleware(['auth']);
-
-Route::get('/kosar', [CartController::class, 'index'] );
-Route::get('/kosarba-rakom/{id}', [CartController::class, 'addToCart'] );
-Route::get('/kosar/{id}/tobb', [CartController::class, 'increase'] );
-Route::get('/kosar/{id}/kevesebb', [CartController::class, 'decrease'] );
-Route::get('/kosar/{id}/torles', [CartController::class, 'destroy'] );
-
-// ------------------- //
-// Felhasználó kezelés //
-// ------------------- //
-
-Route::get('/regisztracio', [RegistrationController::class, 'create'] )
-->middleware(['guest']);
-Route::post('/regisztracio', [RegistrationController::class, 'store'] );
-
-Route::get('/elfelejtett-jelszo', [PasswordRecoveryController::class, 'create'] )
-->middleware(['guest']);
-Route::post('/elfelejtett-jelszo', [PasswordRecoveryController::class, 'store'] );
-
-Route::get('/uj-jelszo/{token}/{email}', [ResetPasswordController::class, 'create'] )
-->middleware(['guest']);
-Route::post('/uj-jelszo', [ResetPasswordController::class, 'store'] );
-
-Route::get('/bejelentkezes', [SessionsController::class, 'create'] )
-->middleware(['guest'])
-->name('bejelentkezes');
-Route::post('/bejelentkezes', [SessionsController::class, 'store'] );
-Route::get('/kijelentkezes', [SessionsController::class, 'destroy'] );
-
-Route::get('/profil', [SessionsController::class, 'index'] )
-->middleware(['auth']);
-// ------------------------ //
-// Felhasználó kezelés vége //
-// ------------------------ //
+Route::get('/termekek', [ProductsController::class, 'index']);
+Route::get('/termekek/{id}', [ProductsController::class, 'show'])->whereNumber('id');
+Route::get('/termekek/{category}', [ProductsController::class, 'list'])->where('category', '[a-z-]+');
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/kosar', [CartController::class, 'index']);
+Route::get('/kosarba-rakom/{id}', [CartController::class, 'addToCart']);
+Route::get('/kosar/{id}/tobb', [CartController::class, 'increase']);
+Route::get('/kosar/{id}/kevesebb', [CartController::class, 'decrease']);
+Route::get('/kosar/{id}/torles', [CartController::class, 'destroy']);
 
-require __DIR__.'/auth.php';
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/regisztracio', [RegistrationController::class, 'create']);
+    Route::post('/regisztracio', [RegistrationController::class, 'store']);
+    Route::get('/elfelejtett-jelszo', [PasswordRecoveryController::class, 'create']);
+    Route::post('/elfelejtett-jelszo', [PasswordRecoveryController::class, 'store']);
+    Route::get('/uj-jelszo/{token}/{email}', [ResetPasswordController::class, 'create']);
+    Route::post('/uj-jelszo', [ResetPasswordController::class, 'store']);
+    Route::get('/bejelentkezes', [SessionsController::class, 'create'])->name('bejelentkezes');
+    Route::post('/bejelentkezes', [SessionsController::class, 'store']);
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/kijelentkezes', [SessionsController::class, 'destroy']);
+    Route::get('/profil', [SessionsController::class, 'index']);
+
+    Route::post('/termekek/{id}/velemeny', [OpinionController::class, 'store']);
+    Route::get('/termekek/{id}/velemeny/szerkesztes', [OpinionController::class, 'edit']);
+    Route::post('/termekek/{id}/velemeny/modositas', [OpinionController::class, 'update']);
+    Route::get('/termekek/{id}/velemeny/torles', [OpinionController::class, 'destroy']);
+});
+
+Route::prefix('admin')->middleware(['admin'])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'] );
+    Route::redirect('/', 'admin/dashboard');
+});
 
 
-Route::get('{page}', [PagesController::class, 'show'] );
+
+Route::get('{page}', [PagesController::class, 'show']);
