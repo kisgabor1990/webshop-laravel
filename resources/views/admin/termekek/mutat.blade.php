93bf21094b5f88e7fb0ec828b1b7c4c7a975e3d7
kisgabor1990 <kisgabor90@gmail.com>
2021-05-11 10:58:43 +0200

    Admin - Termékek leírás
    A termékekhez tartozó leírás nagy képernyőn jobb oldalra került, teljes hosszában az oldalnak

--- resources/views/admin/termekek/mutat.blade.php
+++ resources/views/admin/termekek/mutat.blade.php
@@ -3,58 +3,62 @@
 @section('content')
 
     <div class="row">
-        <div class="col-12 col-lg-5 mb-5">
+        <div class="col-12 mb-5">
             <div class="card">
                 <div class="card-header text-center user-select-none h3">{{ $product->id }} - {{ $product->model }} @if ($product->trashed()) (inaktív) @endif</div>
                 <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-hover">
-                            <tbody>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Kategória:</th>
-                                    <td>{{ $product->category->name }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Gyártó:</th>
-                                    <td>{{ $product->brand->name }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Ár:</th>
-                                    <td>{{ $product->price }} Ft.</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Tulajdonságok:</th>
-                                    <td>
-                                            <table>
-                                                @forelse ($product->properties as $property)
-                                                <tr class="@if ($property->trashed()) table-dark @endif">
-                                                    <th class="user-select-none">{{ $property->name }} </th>
-                                                    <td class="ps-3">{{ $property->pivot->value }}</td>
-                                                </tr>
-                                                @empty
-                                                Nincs tulajdonság társítva!
-                                                @endforelse
-                                            </table>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Leírás:</th>
-                                    <td>{{ $product->description }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Termék létrehozva:</th>
-                                    <td>{{ $product->created_at }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Utoljára módosítva:</th>
-                                    <td>{{ $product->updated_at }}</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" class="user-select-none">Törölve:</th>
-                                    <td>{{ $product->deleted_at ? 'Igen' : 'Nem' }}</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="row">
+                        <div class="col-12 col-lg-5">
+                            <div class="table-responsive">
+                                <table class="table table-hover">
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Kategória:</th>
+                                            <td>{{ $product->category->name }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Gyártó:</th>
+                                            <td>{{ $product->brand->name }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Ár:</th>
+                                            <td>{{ $product->price }} Ft.</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Tulajdonságok:</th>
+                                            <td>
+                                                    <table>
+                                                        @forelse ($product->properties as $property)
+                                                        <tr class="@if ($property->trashed()) bg-dark text-white @endif">
+                                                            <th class="user-select-none">{{ $property->name }} </th>
+                                                            <td class="ps-3">{{ $property->pivot->value }}</td>
+                                                        </tr>
+                                                        @empty
+                                                        Nincs tulajdonság társítva!
+                                                        @endforelse
+                                                    </table>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Termék létrehozva:</th>
+                                            <td>{{ $product->created_at }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Utoljára módosítva:</th>
+                                            <td>{{ $product->updated_at }}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row" class="user-select-none">Törölve:</th>
+                                            <td>{{ $product->deleted_at ? 'Igen' : 'Nem' }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 offset-lg-1">
+                            <p class="h4 user-select-none">Leírás</p>
+                            {{ $product->description }}
+                        </div>
                     </div>
                 </div>
                 <div class="card-footer text-start">
