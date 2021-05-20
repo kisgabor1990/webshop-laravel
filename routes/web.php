c87134b2cf28bf9c5b1341a686faa49f0024203c
kisgabor1990 <kisgabor90@gmail.com>
2021-05-20 21:08:24 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PagesController::class, 'index']);
 
-Route::redirect('/home', '/');
+Route::redirect('/home', url('/'));
 
 Route::get('/termekek', [ProductsController::class, 'index']);
 Route::get('/termek/{product:slug}', [ProductsController::class, 'show']);
