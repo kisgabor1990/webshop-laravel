317f2909ee5ab84641f6b9265a0e8fad97e6a7c5
kisgabor1990 <kisgabor90@gmail.com>
2021-05-28 17:57:49 +0200

    Update success.blade.php

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -1,5 +1,5 @@
 @if (session()->has('success'))
-<div class="alert alert-success">
+<div class="alert alert-success alert-dismissible fade show">
     @if(is_array(session('success')))
         <ul>
             @foreach (session('success') as $message)
@@ -9,5 +9,6 @@
     @else
         {{ session('success') }}
     @endif
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
 @endif
