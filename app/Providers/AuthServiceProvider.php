0c7bb0bdca65f7b28c61cae794db11fd127468d1
kisgabor1990 <kisgabor90@gmail.com>
2021-05-28 18:31:56 +0200

    Jelszóemlékeztető email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -35,6 +36,17 @@ class AuthServiceProvider extends ServiceProvider
                 ->line('Email címének megerősítéséhez kattintson az alábbi gombra.')
                 ->action('Email cím megerősítése', $url)
                 ->line('Ha nem Ön hozta létre ezt a fiókot, hagyja levelünket figyelmen kívül!')
+                ->salutation('Üdvözlettel: ' . config('app.name') . ' csapata!');
+            });
+            
+            ResetPassword::toMailUsing(function($user, $token) {
+                return (new MailMessage)
+                ->subject('Új jelszó beállítása')
+                ->greeting('Kedves ' . $user->name . '!')
+                ->line('Azért kapta ezt az emailt, mert Ön, vagy valaki az Ön email címével új jelszót igényelt oldalunkon.')
+                ->action('Új jelszó', url('uj-jelszo', [$token, $user->email]))
+                ->line('Az új jelszó beállításához szükséges link ' . config('auth.passwords.'.config('auth.defaults.passwords').'.expire') . ' percig érvényes.')
+                ->line('Ha nem Ön kezdeményezte az új jelszó kérését, akkor további teendője nincs.')
                 ->salutation('Üdvözlettel: ' . config('app.name') . ' csapata!');
         });
     }
