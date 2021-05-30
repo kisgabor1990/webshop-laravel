c09ae4595f2b43b63a3d1171b5bad1ef56f6d694
kisgabor1990 <kisgabor90@gmail.com>
2021-05-31 00:03:34 +0200

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -397,7 +397,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="infoModalLabel"></h5>
+                <h4 class="modal-title fw-bold" id="infoModalLabel"></h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
