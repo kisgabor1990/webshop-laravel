8ff1097fcbb875bcf9d3f5b8ac46a6ba225eba5c
kisgabor1990 <kisgabor90@gmail.com>
2021-07-16 20:09:02 +0200

    Termék leírás CKEditorral

--- resources/views/admin/termekek/szerkeszt.blade.php
+++ resources/views/admin/termekek/szerkeszt.blade.php
@@ -15,9 +15,9 @@
                     <input type="hidden" name="category_id" value="{{ $product->category->id }}">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-lg-4">
                                 @if ($product->category->subCategories->count())
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
                                     <select class="form-select" id="subcategory_id" name="subcategory_id">
                                         @foreach ($product->category->subCategories as $subCategory)
                                             <option value="{{ $subCategory->id }}" @if ($subCategory->id == $product->subCategory?->id) selected @endif>{{ $subCategory->name }}</option>
@@ -26,7 +26,7 @@
                                     <label for="subcategory_id">Alkategória</label>
                                 </div>
                                 @endif
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
                                     <select class="form-select" id="brand_id" name="brand_id">
                                         @foreach ($product->category->brands as $brand)
                                             <option value="{{ $brand->id }}" @if ($brand->id == $product->brand->id) selected @endif>{{ $brand->name }}</option>
@@ -34,15 +34,15 @@
                                     </select>
                                     <label for="brand_id">Gyártó</label>
                                 </div>
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
-                                    <input type="text" class="form-control" id="model" name="model" placeholder="Model"
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
+                                    <input type="text" class="form-control" id="model" name="model" placeholder="Megnevezés"
                                         value="{{ $product->model }}" required>
-                                    <label for="model">Model</label>
+                                    <label for="model">Megnevezés</label>
                                     <div class="invalid-feedback">
-                                        A model megadása kötelező!
+                                        A megnevezés megadása kötelező!
                                     </div>
                                 </div>
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
                                     <input type="text" class="form-control" id="price" name="price" placeholder="Ár"
                                         value="{{ $product->price }}" required>
                                     <label for="price">Ár</label>
@@ -51,7 +51,7 @@
                                     </div>
                                 </div>
                                 @foreach ($product->category->properties as $property)
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto @if ($property->trashed()) text-white @endif">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto @if ($property->trashed()) text-white @endif">
                                     @if ($property->hasList)
                                         <select class="form-select @if ($property->trashed()) bg-dark text-white @endif" id="property_{{ $property->name }}" name="values[{{ $property->id }}][value]">
                                             @foreach ($property->values->sortBy('name') as $value)
@@ -66,13 +66,14 @@
                                             value="{{ $product->properties->find($property->id)?->pivot->value }}" required>
                                         <label for="property_{{ $property->name }}">{{ $property->name }}</label>
                                         <div class="invalid-feedback">
-                                            A(z) {{ $property->name }} megadása kötelező!
+                                            A(z) {{ strtolower($property->name) }} megadása kötelező!
                                         </div>
                                     @endif
                                 </div>
                                 @endforeach
                             </div>
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-lg-8">
+                                <p class="h4 user-select-none my-3 mt-lg-0">Leírás</p>
                                 <div class="col-12 form-floating mb-3 mx-auto">
                                     <textarea class="form-control" placeholder="Leírás" id="description" name="description"
                                         style="height: 200px" required>{{ $product->description }}</textarea>
@@ -84,7 +85,7 @@
                         </div>
                         <p class="h4 user-select-none my-3">Képek</p>
                         <div class="row row-cols-1 row-cols-lg-4">
-                            @forelse ($product->images->sortBy('id') as $image)
+                            @foreach ($product->images->sortBy('id') as $image)
                                 <div class="col mb-3 image_{{ $image->id }}">
                                     <input type="hidden" name="images[]" value="{{ $image->id }}">
                                     <div class="card h-100">
@@ -101,7 +102,7 @@
                                                 </div>
                                               </div>
                                         </div>
-                                        <div class="card-body">
+                                        <div class="card-body row align-items-center">
                                             <img src="{{ url($image->path) }}" class="img-fluid" alt="{{ $product->model }}">
                                         </div>
                                         @if (!$image->isCover)
@@ -113,17 +114,7 @@
                                         @endif
                                     </div>
                                 </div>
-                            @empty
-                                {{-- <div class="col-12 col-lg-4 mb-3">
-                                    <p>Nincsenek képek!</p>
-                                    <label for="cover_image">Borítókép</label>
-                                    <input type="file" class="form-control" id="cover_image" name="new_cover_image" placeholder="Borítókép"
-                                        accept="image/*" required>
-                                    <div class="invalid-feedback">
-                                        A borítókép megadása kötelező!
-                                    </div>
-                                </div> --}}
-                            @endforelse
+                            @endforeach
                         </div>
                         <div class="row">
                             <div class="col-12 col-lg-4 mb-3">
@@ -147,10 +138,44 @@
 @section('scripts')
 <script>
     ClassicEditor
-        .create( document.querySelector( '#description' ) )
-        .catch( error => {
-            console.error( error );
-        } );
+        .create( document.querySelector( '#description' ), {
+            toolbar: {
+					items: [
+						'heading',
+						'|',
+						'alignment',
+						'bold',
+						'italic',
+						'underline',
+						'|',
+						'link',
+						'|',
+						'bulletedList',
+						'numberedList',
+						'|',
+						'indent',
+						'outdent',
+						'|',
+						'insertTable',
+						'mediaEmbed',
+						'horizontalLine',
+						'|',
+						'undo',
+						'redo'
+					]
+				},
+				language: 'hu',
+					licenseKey: '',
+				} )
+				.then( editor => {
+					window.editor = editor;
+				} )
+				.catch( error => {
+					console.error( 'Oops, something went wrong!' );
+					console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
+					console.warn( 'Build id: 59xuwaexfk17-yfb2zuy2g9e9' );
+					console.error( error );
+				} );
 </script>
 
 @endsection
--- resources/views/admin/termekek/uj.blade.php
+++ resources/views/admin/termekek/uj.blade.php
@@ -14,9 +14,9 @@
                     <input type="hidden" name="category_id" value="{{ $category->id }}">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-lg-4">
                                 @if ($category->subCategories->count())
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
                                     <select class="form-select" id="subcategory_id" name="subcategory_id">
                                         @foreach ($category->subCategories as $subCategory)
                                             <option value="{{ $subCategory->id }}">{{ $subCategory->name }}</option>
@@ -25,7 +25,7 @@
                                     <label for="subcategory_id">Alkategória</label>
                                 </div>
                                 @endif
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
                                     <select class="form-select" id="brand_id" name="brand_id">
                                         @foreach ($category->brands as $brand)
                                             <option value="{{ $brand->id }}">{{ $brand->name }}</option>
@@ -33,15 +33,15 @@
                                     </select>
                                     <label for="brand_id">Gyártó</label>
                                 </div>
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
-                                    <input type="text" class="form-control" id="model" name="model" placeholder="Model"
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
+                                    <input type="text" class="form-control" id="model" name="model" placeholder="Megnevezés"
                                         value="{{ old('model') }}" required>
-                                    <label for="model">Model</label>
+                                    <label for="model">Megnevezés</label>
                                     <div class="invalid-feedback">
-                                        A model megadása kötelező!
+                                        A megnevezés megadása kötelező!
                                     </div>
                                 </div>
-                                <div class="col-12 col-lg-8 form-floating mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 form-floating mb-3 mx-auto">
                                     <input type="number" class="form-control" id="price" name="price" placeholder="Ár"
                                         value="{{ old('price') }}" required>
                                     <label for="price">Ár</label>
@@ -51,7 +51,7 @@
                                 </div>
                                 @foreach ($category->properties as $property)
                                     @if ($property->hasList)
-                                    <div class="col-12 col-lg-8 form-floating mb-3 mx-auto @if ($property->trashed()) text-white @endif">
+                                    <div class="col-12 col-lg-10 form-floating mb-3 mx-auto @if ($property->trashed()) text-white @endif">
                                         <select class="form-select @if ($property->trashed()) bg-dark text-white @endif" id="property_{{ $property->name }}" name="values[{{ $property->id }}][value]">
                                             @foreach ($property->values->sortBy('name') as $value)
                                                 <option value="{{ $value->name }}">{{ $value->name }}</option>
@@ -60,18 +60,18 @@
                                         <label for="property_{{ $property->name }}">{{ $property->name }}</label>
                                     </div>
                                     @else
-                                        <div class="col-12 col-lg-8 form-floating mb-3 mx-auto @if ($property->trashed()) text-white @endif">
+                                        <div class="col-12 col-lg-10 form-floating mb-3 mx-auto @if ($property->trashed()) text-white @endif">
                                             <input type="text" class="form-control @if ($property->trashed()) bg-dark text-white @endif" id="property_{{ $property->name }}"
                                                 name="values[{{ $property->id }}][value]"
                                                 placeholder="{{ $property->name }}" required>
                                             <label for="property_{{ $property->name }}">{{ $property->name }}</label>
                                             <div class="invalid-feedback">
-                                                A(z) {{ $property->name }} megadása kötelező!
+                                                A(z) {{ strtolower($property->name) }} megadása kötelező!
                                             </div>
                                         </div>
                                     @endif
                                 @endforeach
-                                <div class="col-12 col-lg-8 mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 mb-3 mx-auto">
                                     <label for="cover_image">Borítókép</label>
                                     <input type="file" class="form-control" id="cover_image" name="cover_image" placeholder="Borítókép"
                                         accept="image/*" required>
@@ -79,13 +79,14 @@
                                         A borítókép megadása kötelező!
                                     </div>
                                 </div>
-                                <div class="col-12 col-lg-8 mb-3 mx-auto">
+                                <div class="col-12 col-lg-10 mb-3 mx-auto">
                                     <label for="images">További képek</label>
                                     <input type="file" class="form-control" id="images" name="images[]" placeholder="További képek"
                                         multiple accept="image/*">
                                 </div>
                             </div>
-                            <div class="col-12 col-lg-6">
+                            <div class="col-12 col-lg-8">
+                                <p class="h4 user-select-none my-3 mt-lg-0">Leírás</p>
                                 <div class="col-12 form-floating mb-3 mx-auto">
                                     <textarea class="form-control" placeholder="Leírás" id="description" name="description"
                                         style="height: 200px" required>{{ old('description') }}</textarea>
@@ -110,10 +111,44 @@
 @section('scripts')
 <script>
     ClassicEditor
-        .create( document.querySelector( '#description' ) )
-        .catch( error => {
-            console.error( error );
-        } );
+        .create( document.querySelector( '#description' ), {
+            toolbar: {
+					items: [
+						'heading',
+						'|',
+						'alignment',
+						'bold',
+						'italic',
+						'underline',
+						'|',
+						'link',
+						'|',
+						'bulletedList',
+						'numberedList',
+						'|',
+						'indent',
+						'outdent',
+						'|',
+						'insertTable',
+						'mediaEmbed',
+						'horizontalLine',
+						'|',
+						'undo',
+						'redo'
+					]
+				},
+				language: 'hu',
+					licenseKey: '',
+				} )
+				.then( editor => {
+					window.editor = editor;
+				} )
+				.catch( error => {
+					console.error( 'Oops, something went wrong!' );
+					console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
+					console.warn( 'Build id: 59xuwaexfk17-yfb2zuy2g9e9' );
+					console.error( error );
+				} );
 </script>
 
 @endsection
