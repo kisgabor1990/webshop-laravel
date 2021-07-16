c1a11ebc248df2484450afafd786fddecfe5a941
kisgabor1990 <kisgabor90@gmail.com>
2021-07-16 10:59:57 +0200

    Figyelmeztető üzenetek (error, success) átalakítása

--- resources/views/alerts/success.blade.php
+++ resources/views/alerts/success.blade.php
@@ -1,14 +1,18 @@
 @if (session()->has('success'))
-<div class="alert alert-success alert-dismissible fade show">
-    @if(is_array(session('success')))
-        <ul>
-            @foreach (session('success') as $message)
-                <li>{{ $message }}</li>
-            @endforeach
-        </ul>
-    @else
-        {{ session('success') }}
-    @endif
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="row">
+    <div class="col-auto">
+        <div class="alert alert-success alert-dismissible fade show">
+            @if(is_array(session('success')))
+                <ul>
+                    @foreach (session('success') as $message)
+                        <li>{{ $message }}</li>
+                    @endforeach
+                </ul>
+            @else
+                {{ session('success') }}
+            @endif
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
 </div>
 @endif
