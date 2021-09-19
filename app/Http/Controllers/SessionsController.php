ccdc11c3b9f33870c08d80b58d0827ff6ffbfbe3
kisgabor1990 <kisgabor90@gmail.com>
2021-09-19 21:12:54 +0200

    Update SessionsController.php

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,52 +15,48 @@ use function view;
 
 class SessionsController extends Controller
 {
-    public function index()
-    {
+    public function index() {
         $user = Auth::user();
 
         return view('auth.profil')->with('user', $user);
     }
 
-    public function create()
-    {
+    public function create() {
         return view('auth.bejelentkezes');
     }
 
-    public function store(LoginRequest $request)
-    {
-        if (!session()->has('url.intended')) {
+    public function store(LoginRequest $request) {
+        if ( ! session()->has('url.intended')) {
             session(['url.intended' => url()->previous()]);
         }
-        if (!(Auth::attempt($request->only('email', 'password')))) {
+        if ( ! (Auth::attempt($request->only('email', 'password')))) {
             return redirect()->to("/bejelentkezes")->withErrors(['message' => 'Hibás email/jelszó páros!']);
         }
 
         $user = User::find(Auth::id());
         $user->last_login = date("Y-m-d H:i:s");
         $user->save();
-        
+
         if ($cart = session()->get('cart')) {
             foreach ($cart as $key => $product) {
-                
+
                 $user->carts()->updateOrCreate(
                     ['product_id' => $key],
                     ['quantity' => DB::raw('quantity + ' . $product['quantity'])]
                 );
             }
         }
-        
+
         $request->session()->regenerate();
 
         if ($user->password_must_change == 1) {
             return redirect("/profil/jelszo-modositas");
         }
-        
+
         return redirect()->intended();
     }
 
-    public function destroy(Request $request)
-    {
+    public function destroy(Request $request) {
         Auth::logout();
 
         $request->session()->invalidate();
@@ -70,8 +66,11 @@ class SessionsController extends Controller
         return redirect()->to('/');
     }
 
-    public function createPasswordChange()
-    {
+    public function createPasswordChange() {
         return view('auth.jelszo-modositas');
+    }
+
+    public function storePasswordChange() {
+
     }
 }
