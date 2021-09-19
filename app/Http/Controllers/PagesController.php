c68b87c3fc255d7f2d24656bdcc274176d8b8652
kisgabor1990 <kisgabor90@gmail.com>
2021-09-19 21:12:48 +0200

    Update PagesController.php

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,21 +7,24 @@ use App\Models\Product;
 use function resource_path;
 use function view;
 
-class PagesController extends Controller {
+class PagesController extends Controller
+{
 
     public function index() {
-        $newest = Product::with(['images', 'brand', 'properties', 'ratings'])->whereHas('brand', function($q) {$q->whereNull('deleted_at');})->orderByDesc('id')->limit(8)->get();
+        $newest = Product::with(['images', 'brand', 'properties', 'ratings'])->whereHas('brand', function ($q) {
+            $q->whereNull('deleted_at');
+        })->orderByDesc('id')->limit(8)->get();
         return view('pages.home')->with('newest', $newest);
     }
 
     public function show($page) {
 
-        if (!is_file(resource_path() . '/views/pages/' . $page . '.blade.php')) {
+        if ( ! is_file(resource_path() . '/views/pages/' . $page . '.blade.php')) {
             $page = '404';
         }
 
 
         return view('pages.' . $page);
     }
-    
+
 }
